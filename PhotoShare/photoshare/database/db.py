150c41a58ddd20e8c7f93b8d87cfb2dbe0898fe8
Nazar Chaban <134311145+Vivern0@users.noreply.github.com>
2024-05-16 21:16:41 +0300

    db, models, docker-compose, gitignore

---
+++ PhotoShare/photoshare/database/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://useradmin:supersecretpassword@localhost:5432/FastAPI_APP'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://useradmin:supersecretpassword@localhost:5432/FastAPI_APP'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Сесія для тестів моделей
+# ПОТІМ ТРЕБА ПРИБРАТИ
+test_session = LocalSession()
+
+def get_db():
+    db = LocalSession()
+    try:
+        yield db
+    finally:
+        db.close()
