744efb0659d1db1b96e071c1b2e83c24ea4933f3
Misha-HF <144258555+Misha-HF@users.noreply.github.com>
2024-05-18 13:30:43 +0300

    fromat-api

--- PhotoShare/photoshare/repository/images.py
+++ PhotoShare/photoshare/repository/images.py
@@ -1,6 +1,6 @@
-from database.db import Session
-from database.models import Image
-from schemas import ImageBase
+from photoshare.database.db import Session
+from photoshare.database.models import Image
+from photoshare.schemas import ImageBase
 
 def load_image_func(db: Session, image: ImageBase):
     db_image = Image(**image.dict())
